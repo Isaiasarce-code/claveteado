6c40e087e0ac665c080e09d9a1cd9e9dd4f7a96e
Isaiasarce-code <isaiasarce321@gmail.com>
2025-04-29 11:44:17 -0600

    Update claveteado.py

--- claveteado.py
+++ claveteado.py
@@ -2,16 +2,7 @@ import streamlit as st
 import pandas as pd
 from rapidfuzz import process, fuzz
 
-
-st.set_page_config(
-    page_title="CLAVETEADO DE UNIDADES ANA SEGUROS",
-    layout="wide",
-    menu_items={
-        "Get Help": None,
-        "Report a bug": None,
-        "About": None
-    }
-)
+st.set_page_config(layout="wide")
 # Estilo personalizado: fondo rojo y texto blanco
 st.markdown("""
     <style>
